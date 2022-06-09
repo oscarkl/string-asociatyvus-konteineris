9167476e3f74f5db0704c75e296271ceeb2b0056
oscarkl <42514691+oscarkl@users.noreply.github.com>
2022-06-09 18:08:12 +0300

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iterator>
+#include <set>
+#include <iomanip>
+#include <algorithm>
+#include <regex>
+
+using std::ifstream;
+using std::ofstream;
+using std::string;
+using std::cout;
+using std::endl;
+using std::getline;
+using std::stringstream;
+using std::multiset;
+using std::pair;
+using std::vector;
+using std::left;
+using std::setw;
+
+char mazosios(const char& c);
+void process_word(string& word);
+bool isletter(const char& r);
+bool isword(const string& word);
+bool isurl(const string& word);
