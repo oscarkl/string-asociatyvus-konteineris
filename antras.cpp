74c2e75652e7ce81cf1278cf476fc85b0a3eea08
oscarkl <42514691+oscarkl@users.noreply.github.com>
2022-06-09 18:49:28 +0300

    Update antras.cpp

--- antras.cpp
+++ antras.cpp
@@ -11,9 +11,9 @@ char mazosios(const char& c) {
     return std::tolower(c);
 }
 
-void process_word(string& word) {
-    string symbols = "(),.[]-„“\"\'\\:;%!?/<>|{}+=-*&^%$#@~`”•€–0123456789°½¢£";
-
+void besymbol(string& word) {
+    string symbols = "(),.[]-„“\"\':;%!?/<>|\\{}+=-*&^%$#@~`”•€–0123456789°½¢£";
+    string quote = "\"";
     while (symbols.find(*(word.end() - 1)) < symbols.size()) {
         for (char c : symbols) {
             if (*(word.begin()) == c)
@@ -23,7 +23,16 @@ void process_word(string& word) {
                 word.erase(word.end() - 1);
         }
     }
-
+        for (char c : symbols) {
+            if (*(word.begin()) == c)
+            {
+                word.erase(word.begin());
+            }
+            if (*(word.end() - 1) == c)
+            {
+                word.erase(word.end() - 1);
+            }
+        }
 }
 bool isletter(const char& r) {
     string abecele = "aąbcčdeęėfghiįyjklmnoprsštuųūvzž";
@@ -34,8 +43,20 @@ bool isletter(const char& r) {
     }
     return false;
 }
+bool isnumber(const char& r) {
+    string numb = "0123456789";
+    //string symbols = "";
+    for (char l : numb) {
+        if (r == l)
+            return true;
+    }
+    return false;
+}
 bool isword(const string& word) {
     for (char r : word) {
+        if (isnumber(r)) {
+            return false;
+        }
         if (isletter(r)) {
             return true;
         }
